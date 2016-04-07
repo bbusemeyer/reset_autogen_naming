627b0e87ffc8e512143baad1df2e194b9fe4d39d
Brian Busemeyer <bbusemeyer@gmail.com>
2016-04-07 10:11:41 -0500

    optimization renaming works

--- reset_naming.py
+++ reset_naming.py
@@ -29,7 +29,7 @@ def find_only_int(strlist):
       if found_already:
         print("Two ints in list. Unambiguous result.")
         print(strlist)
-        sys.exit("Unrecoverable error.")
+        raise AssertionError
       else:
         found_already = True
     except ValueError:
@@ -37,12 +37,14 @@ def find_only_int(strlist):
   if res == -10000000:
     print("Found no ints in this list.")
     print(strlist)
-    sys.exit("Unrecoverable error.")
+    raise AssertionError
   return res
 
 # Copied from runqwalk.py.
 def gen_basename(k,t,loc,jast,opt):
   return "qw_%i_%s_%g_%s_%s"%(k,jast,t,opt,loc)
+def gen_optbase(k,jast):
+  return "qw_%i.%s"%(k,jast)
 
 def reset_naming(ag_dir):
   """
@@ -59,8 +61,14 @@ def reset_naming(ag_dir):
 
   # Move all files that require new names to be consistent with new scheme.
   flist = os.listdir(ag_dir)
-  dmclist = [f for f in flist if ".dmc" in f]
-  dmcbase = [f[:f.find(".dmc")] for f in dmclist]
+  flist = [f for f in flist if "stdout" not in f]
+  dmclist   = [f for f in flist if ".dmc" in f]
+  enoptlist = [f for f in flist if ("enopt" in f) and ("qw" in f)]
+  optlist   = [f for f in flist if ("opt" in f) and ("qw" in f) and (f not in enoptlist)]
+  print(optlist)
+  dmcbase   = [f[:f.find(".dmc")]   for f in dmclist]
+  enoptbase = [f[:f.find(".enopt")] for f in enoptlist]
+  optbase   = [f[:f.find(".opt")]   for f in optlist]
   for didx, dmcfn in enumerate(dmclist):
     print("Old: %s"%dmcfn)
     kpt = find_only_int(dmcbase[didx].split('_'))
@@ -74,6 +82,26 @@ def reset_naming(ag_dir):
       ))
     print("New: %s"%newfn)
     sh.move('/'.join((ag_dir,dmcfn)),'/'.join((ag_dir,newfn)))
+
+  for oidx, optfn in enumerate(optlist):
+    print("Old: %s"%optfn)
+    if "_0" not in optfn: 
+      print("I'm only set to deal with gamma-point optimziations!")
+      raise AssertionError
+    newfn = optfn.replace(optbase[oidx], gen_optbase(0,dat['jastrow']))
+    print("New: %s"%newfn)
+    sh.move('/'.join((ag_dir,optfn)),'/'.join((ag_dir,newfn)))
+
+  for oidx, enoptfn in enumerate(enoptlist):
+    print("Old: %s"%enoptfn)
+    if "_0" not in enoptfn: 
+      print("I'm only set to deal with gamma-point optimziations!")
+      raise AssertionError
+    newfn = enoptfn.replace(enoptbase[oidx], gen_optbase(0,dat['jastrow']))
+    print("New: %s"%newfn)
+    sh.move('/'.join((ag_dir,enoptfn)),'/'.join((ag_dir,newfn)))
+
+  print("Finished renaming successfully.")
 
 if __name__ == "__main__":
   if len(sys.argv) < 2:
