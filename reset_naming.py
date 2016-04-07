673f0feac3c7f3994cec8402c94b5b782033def1
Brian Busemeyer <bbusemeyer@gmail.com>
2016-04-07 09:11:43 -0500

    First commit.

---
+++ reset_naming.py
@@ -0,0 +1,82 @@
+import sys
+import os
+import json
+import shutil as sh
+
+def get_parm(parmkey,record):
+  """ Helper function for reset_naming().
+  Gets parameter from record.json dictionary."""
+  parm = ""
+  if parmkey in record['qmc']['dmc'].keys():
+    parm = record['qmc']['dmc'][parmkey]
+  else:
+    sys.exit("Don't know how to handle this key: %s ."%parmkey)
+  if type(parm) == list:
+    if len(parm)>1:
+      print("Error: you used multiple %s,"%parmkey,)
+      print("and I don't know how to seperate which is which.")
+      sys.exit()
+    parm = parm[0]
+  return parm
+
+def find_only_int(strlist):
+  """ Helper: finds int in list of strings, error if there not exactly one. """
+  res = -10000000
+  found_already = False
+  for item in strlist:
+    try:
+      res = int(item)
+      if found_already:
+        print("Two ints in list. Unambiguous result.")
+        print(strlist)
+        sys.exit("Unrecoverable error.")
+      else:
+        found_already = True
+    except ValueError:
+      pass
+  if res == -10000000:
+    print("Found no ints in this list.")
+    print(strlist)
+    sys.exit("Unrecoverable error.")
+  return res
+
+# Copied from runqwalk.py.
+def gen_basename(k,t,loc,jast,opt):
+  return "qw_%i_%s_%g_%s_%s"%(k,jast,t,opt,loc)
+
+def reset_naming(ag_dir):
+  """
+  Reset naming of autogen files to be consistent with existing record.json and
+  whatever naming scheme is hard coded in here.
+  """
+  # Read in and extract data from record.json.
+  record = json.load(open("/".join((ag_dir,"record.json"))))
+
+  dat = {}
+  for parmkey in ['timestep','optimizer','jastrow','localization']:
+    dat[parmkey] = get_parm(parmkey,record)
+
+
+  # Move all files that require new names to be consistent with new scheme.
+  flist = os.listdir(ag_dir)
+  dmclist = [f for f in flist if ".dmc" in f]
+  dmcbase = [f[:f.find(".dmc")] for f in dmclist]
+  for didx, dmcfn in enumerate(dmclist):
+    print("Old: %s"%dmcfn)
+    kpt = find_only_int(dmcbase[didx].split('_'))
+    newfn = dmcfn.replace(dmcbase[didx],
+      gen_basename(
+        kpt, 
+        float(dat['timestep']),
+        dat['localization'],
+        dat['jastrow'],
+        dat['optimizer']
+      ))
+    print("New: %s"%newfn)
+    sh.move('/'.join((ag_dir,dmcfn)),'/'.join((ag_dir,newfn)))
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print("Usage: python reset_naming.py <list of dirs>")
+  for ag_dir in sys.argv[1:]:
+    reset_naming(ag_dir)
